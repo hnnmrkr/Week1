28573a75c0fc22c5c0349b3534eddd06497b148f
Hanna <hanna-marii.kriisa@voco.ee>
2023-09-15 09:50:03 +0300

    Exercise nr 5

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+  public static void main(String[] args) {
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    // Exercise number 5 - Create a program that counts how many seconds there are in a year.
+    int days = 365;
+    int secondsInHour = 60 * 60;
+    int secondsInDay = secondsInHour * 24;
+    int secondsInYear = secondsInDay * days;
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
-    }
+    System.out.println("There are " + secondsInYear + " seconds in a year.");
+
+    // Reading user input
+    Scanner reader = new Scanner(System.in);
+
+    System.out.println("What is your name? ");
+    String name = reader.nextLine(); // User writes a name in console
+
+    System.out.println("Hi, " + name);
+  }
 }
